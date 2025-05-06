8046822f87ac5c54bd724c9108eb48835ca25c50
DocStrenzh <mishaprokhorov2014@yandex.ru>
2025-05-06 14:19:24 +0300

    Made a proper text slide

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+    { text: "Ideas", ingPath: "/images/ideas.svg" },
+    { text: "Concepts", ingPath: "/images/concepts.svg" },
+    { text: "Designs", ingPath: "/images/designs.svg" },
+    { text: "Code", ingPath: "/images/code.svg" },
+    { text: "Ideas", ingPath: "/images/ideas.svg" },
+    { text: "Concepts", ingPath: "/images/concepts.svg" },
+    { text: "Designs", ingPath: "/images/designs.svg" },
+    { text: "Code", ingPath: "/images/code.svg" },
+]
