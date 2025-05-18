36917d3645bb9d9612aa563eb8dfe45d31cd38f2
DocStrenzh <mishaprokhorov2014@yandex.ru>
2025-05-19 00:33:41 +0300

    Fixed a small navbar bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,47 +2,47 @@ import React, {useEffect, useState} from 'react';
 import { navLinks } from "../constants/index.js";
 
 const NavBar = () => {
-    const [scrolled, setScrolled] = useState(false)
+  const [scrolled, setScrolled] = useState(false)
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const isScrolled = window.scrollY > 10
-            setScrolled(true)
-        }
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10
+      setScrolled(isScrolled)
+    }
 
-        window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll)
 
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, []);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, []);
 
-    return (
-        <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
-            <div className="inner">
-                <a className="logo" href="#hero">
-                    Мигель | Pro
+  return (
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
+      <div className="inner">
+        <a className="logo" href="#hero">
+          Мигель | Pro
+        </a>
+
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name}) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
                 </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-                <nav className="desktop">
-                    <ul>
-                        {navLinks.map(({ link, name}) => (
-                            <li key={name} className="group">
-                                <a href={link}>
-                                   <span>{name}</span>
-                                    <span className="underline" />
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-
-                <a href="#contact" className="contact-btn group">
-                    <div className="inner">
-                        <span>Напиши мне(х3)</span>
-                    </div>
-                </a>
-            </div>
-        </header>
-    );
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Напиши мне(х3)</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
